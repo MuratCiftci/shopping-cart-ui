dbe88006e062ef2273cd8bfbe1365ac39c6a285d
MuratCiftci <mciftci174@posta.pau.edu.tr>
2021-11-24 00:02:59 +0300

    navbar changed

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,15 +1,15 @@
 import styles from "./Navigation.module.css"
+import Link from 'next/link'
+import slug from 'slug'
 function Navigation() {
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
                 <ul className={styles.menu}>
-                    <li className={styles.menuItem}>Elektronik</li>
-                    <li className={styles.menuItem}>Moda</li>
-                    <li className={styles.menuItem}>Ev</li>
-                    <li className={styles.menuItem}>Bahçe</li>
-                    <li className={styles.menuItem}>Oyuncak</li>
-                    <li className={styles.menuItem}>Kozmetik</li>
+                    <li  className={styles.menuItem}><Link href="/category/[category]" as={"/category/electronics"}>Elektronik</Link></li>
+                    <li  className={styles.menuItem}><Link href="/category/[category]" as={"/category/jewelery"}>Aksesuar</Link></li>
+                    <li  className={styles.menuItem}><Link href="/category/[category]" as={"/category/men's clothing"}>Erkek Giyim</Link></li>
+                    <li  className={styles.menuItem}><Link href="/category/[category]" as={"/category/women's clothing"}>Kadın Giyim</Link></li>
                 </ul>
             </div>
         </div>
