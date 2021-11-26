611fdac61e731de1c6e7352e77d3f1c4ea55c16a
MuratCiftci <mciftci174@posta.pau.edu.tr>
2021-11-26 23:33:13 +0300

    mini cart added

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,18 +1,27 @@
 import styles from "./Navbar.module.css";
-import {CartContext} from '../../context/CartContext'
+import { CartContext } from "../../context/CartContext";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Badge from "@mui/material/Badge";
 import Link from "next/link";
-import {useContext} from 'react'
+import { useContext, useState } from "react";
+import { getCookie } from "../../lib/useCookie";
+import MiniCart from "../Cart/MiniCart";
 function Navbar() {
-  const {cartItems} = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+
+  const [showCart, setShowCart] = useState(false);
+  console.log(showCart);
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.left}>
-          <Link href="/"><a><h1>Shoppy</h1></a></Link>
+          <Link href="/">
+            <a>
+              <h1>Shoppy</h1>
+            </a>
+          </Link>
         </div>
         <div className={styles.center}>
           <div className={styles.SearchContainer}>
@@ -26,9 +35,16 @@ function Navbar() {
         <div className={styles.right}>
           <div className={styles.MenuItem}>REGISTER</div>
           <div className={styles.MenuItem}>SIGN IN</div>
-          <div className={styles.MenuItem}>
-            <Badge badgeContent={cartItems.length} color="primary"></Badge>
-            <Link href="/cart"><ShoppingCartIcon /></Link>
+          <div
+            className={styles.MenuItem}
+            onMouseEnter={() => setShowCart(true)}
+            onMouseLeave={() => setShowCart(false)}
+          >
+            <div className={styles.cartCount}>{cartItems.length}</div>
+            <Link href="/cart">
+              <ShoppingCartIcon />
+            </Link>
+            {showCart && <MiniCart />}
           </div>
         </div>
       </div>
